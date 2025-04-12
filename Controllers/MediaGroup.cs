9282167106ed7b7d9aaade071a552894159b0c86
uodrone <drone_86@mail.ru>
2025-04-12 08:17:16 +0400

    Корректировки комментов

--- Controllers/MediaGroup.cs
+++ Controllers/MediaGroup.cs
@@ -1,10 +1,12 @@
 ﻿public class MediaGroup
 {
-    public string MediaGroupId { get; } // ID медиагруппы
-    public List<(long ChatId, string FileId)> Files { get; } = new List<(long ChatId, string FileId)>(); // Список файлов
-    public DateTime LastMessageTime { get; private set; } = DateTime.UtcNow; // Время последнего сообщения
-    public bool IsProcessing { get; set; } = false; // Флаг обработки
-    public string Caption { get; set; } // Текст сообщения (подпись)
+    public string MediaGroupId { get; }
+    public List<(long ChatId, string FileId)> Files { get; } = new List<(long ChatId, string FileId)>();
+    public DateTime LastMessageTime { get; private set; } = DateTime.UtcNow;
+    // Флаг обработки
+    public bool IsProcessing { get; set; } = false;
+    // Текст сообщения (подпись)
+    public string Caption { get; set; }
 
     public MediaGroup(string mediaGroupId)
     {
@@ -15,7 +17,7 @@
     public void AddFile(long chatId, string fileId)
     {
         Files.Add((chatId, fileId));
-        LastMessageTime = DateTime.UtcNow; // Обновляем время последнего сообщения
+        LastMessageTime = DateTime.UtcNow;
     }
 
     // Проверяем, завершена ли медиагруппа (прошло ли более 500 мс с последнего сообщения)
--- Controllers/RegularValidation.cs
+++ Controllers/RegularValidation.cs
@@ -7,23 +7,17 @@ namespace HRProBot.Controllers
         public bool ValidateName(string name)
         {
             // Регулярное выражение для имени и фамилии:
-            // - Допустимы только буквы (латиница или кириллица)
-            // - Длина от 2 до 50 символов
             string pattern = @"^[a-zA-Zа-яА-Я]{2,100}$";
             return Regex.IsMatch(name, pattern);
         }
 
-        // Валидация организации
         public bool ValidateOrganization(string org)
         {
             // Регулярное выражение для организации:
-            // - Допустимы буквы, цифры, пробелы и дефисы
-            // - Длина от 2 до 100 символов
             string pattern = @"^.{2,300}$";
             return Regex.IsMatch(org, pattern);
         }
 
-        // Валидация телефона
         public bool ValidatePhone(string phone)
         {
             // Регулярное выражение для телефона:
